e81a78fc9566733ce9dd32ce1a1f6d6c8fa6c541
gumengyin <gumegyin@163.com>
2016-10-20 00:20:41 +0800

    homework10.18

---
+++ homework10.18/3-1.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  3-1
+//
+//  Created by 顾梦寅Joyce on 16/10/19.
+//  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int a,b,c;
+    printf("请将三个实数输入到变量a,b,c\n");
+    printf("a=");
+    scanf("%d",&a);
+    printf("b=");
+    scanf("%d",&b);
+    printf("c=");
+    scanf("%d",&c);
+    return 0;
+}
