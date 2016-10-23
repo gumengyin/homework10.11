d0ef1ad329ca0a726aeeca93e8ca45cf0bc74833
gumengyin <gumegyin@163.com>
2016-10-23 19:57:25 +0800

    考虑大括号的作用

---
+++ homework10.18/3-2(version2).c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  3-2(version2)
+//
+//  Created by 顾梦寅Joyce on 16/10/23.
+//  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
+//注意命名的浮点数最好有小数位
+
+#include <stdio.h>
+int main(void)
+{
+    double a;
+    printf("请输入成绩\n");
+    scanf("%lf",&a);
+    if (90.0<=a&&a<=100.0)
+    {
+        printf("A\n");
+    }
+    else if (80.0<=a&&a<=89.0)
+    {
+        printf("B\n");
+    }
+    else if (70.0<=a&&a<=79.0)
+    {
+        printf("C\n");
+    }
+    else if (60.0<=a&&a<=69.0)
+    {
+        printf("D\n");
+    }
+    else
+    {
+        printf("E\n");
+    }
+    
+}
--- homework10.18/3-2.c
+++ homework10.18/3-2.c
@@ -5,7 +5,7 @@
 //  Created by 顾梦寅Joyce on 16/10/22.
 //  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
 
-//失败了不能正常运行
+//reason for failure:不能连续判断90<=a<=100应为90<=a&&a<=100
 
 #include <stdio.h>
 int main(void)
@@ -13,25 +13,25 @@ int main(void)
     double a;
     printf("请输入成绩\n");
     scanf("%lf",&a);
-    if (90<=a<=100)
+    if (90<=a&&a<=100)
     {
         printf("A\n");
     }
     else
     {
-        if (80<=a<=89)
+        if (80<=a&&a<=89)
         {
             printf("B\n");
         }
         else
         {
-            if (70<=a<=79)
+            if (70<=a&&a<=79)
             {
                 printf("C\n");
             }
             else
             {
-                if (60<=a<=69)
+                if (60<=a&&a<=69)
                 {
                     printf("D\n");
                 }
