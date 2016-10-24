c22cd0d212af8fa642bb13fb970340fc8fac711c
gumengyin <gumegyin@163.com>
2016-10-25 00:17:21 +0800

    闰年数

---
+++ homework10.18/3-14.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  3-14
+//
+//  Created by 顾梦寅Joyce on 16/10/25.
+//  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int a,b=0;
+    for(a=2000;a<=3000;a++)
+    {
+        if(a%4==0)
+            b=b+1;
+    }
+    printf("2000-3000年间的闰年数%d\n",b);
+    return 0;
+}
