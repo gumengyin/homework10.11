777ee9b7d4e8f95658e138e9e3b15b5c040a0443
gumengyin <gumegyin@163.com>
2016-10-25 14:49:30 +0800

    闰年

--- homework10.18/3-14.c
+++ homework10.18/3-14.c
@@ -4,7 +4,7 @@
 //
 //  Created by 顾梦寅Joyce on 16/10/25.
 //  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
-//
+//闰年：4整除并且100不整除 或 400整除
 
 #include <stdio.h>
 
@@ -12,7 +12,8 @@ int main(void) {
     int a,b=0;
     for(a=2000;a<=3000;a++)
     {
-        if(a%4==0)
+        if((a%4==0&&a%100!=0)||(a%400==0))
+            printf("%d是闰年\n",a);
             b=b+1;
     }
     printf("2000-3000年间的闰年数%d\n",b);
