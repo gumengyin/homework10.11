05fa075efeace5b318b372644fb1c1785ba86d3d
gumengyin <gumegyin@163.com>
2016-10-26 23:35:46 +0800

    复利（失败）

---
+++ homework10.18/3-15.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  3-15
+//
+//  Created by 顾梦寅Joyce on 16/10/25.
+//  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    double a,s;
+    int i;
+    do
+    {
+        i=0;
+        a=0;
+        s=a+5000;
+        a=s+0.32*s;
+        i=i+1;
+    }while (s<=600000||a<=600000); //注意分号;while成立则循环
+    {
+        printf("需要%d年\n",i/4);
+    }
+    return 0;
+}
