9701fc7ef1c2fe2fc8aad7e748d50c0d2d589fdf
gumengyin <gumegyin@163.com>
2016-10-31 23:46:06 +0800

    算两数间奇数和

---
+++ 上机课/算两数之间奇数和.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  算两数之间奇数和
+//
+//  Created by 顾梦寅Joyce on 2016/10/31.
+//  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
+//
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    int a, b,c;
+    printf("请选择需要输入的数据组数\n");
+    scanf("%d", &n);
+    printf("输入   输出\n",n);
+    for (i = 0; i < n; i++)
+    {
+        int d=0;
+        scanf("%d %d",&a,&b);
+        for (c=a;c<=b;c++)
+        {
+            if (c%2!=0)
+                d=d+c;
+        }
+        printf("        %d",d);
+    }
+    return 0;
+    
+}
