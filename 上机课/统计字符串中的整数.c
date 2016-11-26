e1e2d2af74aadf5e1dd3c3a15fd3e88edaafad9c
gumengyin <gumegyin@163.com>
2016-11-27 00:28:27 +0800

    success

--- 上机课/统计字符串中的整数.c
+++ 上机课/统计字符串中的整数.c
@@ -10,77 +10,70 @@
 #include <string.h>
 int main()
 {
-    char str[50],*pstr;
-    int i=0,j=0,k,m,n=0,e10,digit,ndigit=0,a[100],*pa;
-    while(gets(str)!=NULL){
-        pstr=&str[0];/*字符串指针指向字符串首地址*/
-        pa=&a[0];/*数字指针指向数组首地址*/
-        for (i=0; i<strlen(str); i++) {
-            if (str[i]>=48&&str[i]<=57) {
-                n=n+1;
-            }
-        }
-        if (n==strlen(str)) {
-            printf("1||");
-            for (i=0; i<strlen(str); i++) {
-                printf("%c",str[i]);
-            }
-        }
-        else  {
-            while(*(pstr+i)!='\0')
+    int n;
+    for (n=0; n<100; n++) {
+        
+        char str[50],*pstr;
+        int i=0,j=0,k,m,ndigit=0;
+        long int e10,digit,a[100],*pa;
+        gets(str);
+        pstr=&str[0];
+        pa=&a[0];
+        while(*(pstr+i)!='\0')
+        {
+            if((*(pstr+i)>='0')&&(*(pstr+i)<='9'))
+                j++;
+            else
             {
-                if((*(pstr+i)>='0')&&(*(pstr+i)<='9'))/*如果是数字，则开始计数*/
-                    j++;
-                else
+                if(j>0)
                 {
-                    if(j>0)/*当上一个字符是数字，当前字符是非数字时*/
+                    digit=*(pstr+i-1)-48;
+                    k=1;
+                    while(k<j)
                     {
-                        digit=*(pstr+i-1)-48;/*先计算出最后一个数字*/
-                        k=1;
-                        while(k<j)/*依次计算其他位上的数字*/
-                        {
-                            e10=1;
-                            for(m=1;m<=k;m++)/*按照数字应有的位置乘以相应的幂次方*/
-                                e10*=10;
-                            digit+=(*(pstr+i-1-k)-48)*e10;
-                            k++;
-                        }
-                        *pa=digit;/*将本次计算出来的数字存入数组*/
-                        ndigit++;/*数字个数增1*/
-                        pa++;
-                        j=0;
-                    }
-                }
-                i++;/*字符串指针往后移动一位*/
-            }
-            if(j>0)/*如果字符串是以数字结束的*/
-            {
-                digit=*(pstr+i-1)-48;
-                k=1;
-                while(k<j){
-                    e10=1;
-                    for(m=1;m<=k;m++){
-                        e10*=10;
+                        e10=1;
+                        for(m=1;m<=k;m++)
+                            e10*=10;
                         digit+=(*(pstr+i-1-k)-48)*e10;
                         k++;
                     }
                     *pa=digit;
                     ndigit++;
+                    pa++;
                     j=0;
                 }
             }
-            printf("%d||",ndigit);
-            j=0;
-            pa=&a[0];
-            if (ndigit==0) {
-                printf("NULL");
-            }
-            for(j=0;j<ndigit;j++){/*输出数字*/
-                printf("%d ",*(pa+j));
-            }
-            printf("\n");
+            i++;
         }
+        
+        if(j>0)
+        {
+            digit=*(pstr+i-1)-48;
+            k=1;
+            while(k<j)
+            {
+                e10=1;
+                for(m=1;m<=k;m++)
+                    e10*=10;
+                digit+=(*(pstr+i-1-k)-48)*e10;
+                k++;
+            }
+            *pa=digit;
+            ndigit++;
+            j=0;
+        }
+        
+        printf("%d||",ndigit);
+        j=0;
+        pa=&a[0];
+        if (ndigit==0) {
+            printf("NULL");
+        }
+        else
+            for(j=0;j<ndigit;j++)
+                printf("%ld ",*(pa+j));
+        printf("\n");
     }
-    
     return 0;
 }
+
