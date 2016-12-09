711f679f4fd52eb1b467d15b6d48f8551437a2bc
gumengyin <gumegyin@163.com>
2016-12-10 01:42:02 +0800

    删除子串 解决一些bug

--- 上机课/删除子串.c
+++ 上机课/删除子串.c
@@ -6,6 +6,14 @@
 //  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
 //
 
+//
+//  main.c
+//  删除子串
+//
+//  Created by 顾梦寅Joyce on 2016/12/5.
+//  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
+//
+
 #include <stdio.h>
 #include <string.h>
 int main() {
@@ -15,7 +23,12 @@ int main() {
     gets(str2);
     for (i=0; i<strlen(str1);i++) {
         for (j=0; j<strlen(str2); j++) {
-            if ((str1[i]==str2[j])&&((str1[i+1]==str2[j+1])||(str1[i-1]==str2[j-1]))) {
+            if (strlen(str2)==1) {
+                if (str1[i]==str2[j]) {
+                    m=m+1;
+                }
+            }
+            else if ((str1[i]==str2[j])&&((str1[i+1]==str2[j+1])||(str1[i-1]==str2[j-1]))) {
                 m=m+1;
             }
         }
@@ -29,11 +42,20 @@ int main() {
     else{
         j=0;
         for (i=0; i<strlen(str1);i++) {
-            if ((str1[i]==str2[j])&&((str1[i+1]==str2[j+1])||(str1[i-1]==str2[j-1]))) {
-                j=j+1;
+            if (strlen(str2)==1) {
+                if (str1[i]==str2[j]) {
+                    j=j+1;
+                }
+                else
+                    printf("%c",str1[i]);
             }
-            else
-                printf("%c",str1[i]);
+            else {
+                if ((str1[i]==str2[j])&&((str1[i+1]==str2[j+1])||(str1[i-1]==str2[j-1]))) {
+                    j=j+1;
+                }
+                else
+                    printf("%c",str1[i]);
+            }
         }
     }
     return 0;
