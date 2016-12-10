c0b49d649bad269a290940d0f1c3dd1a9d4eb27c
gumengyin <gumegyin@163.com>
2016-12-10 13:48:55 +0800

    还差点

--- 上机课/统计该子串在母串中出现的次数.c
+++ 上机课/统计该子串在母串中出现的次数.c
@@ -16,47 +16,42 @@
 
 #include <stdio.h>
 #include <string.h>
-int FUN(int k,char str1[],char str2[]){
-    int i,j=0,m=0;
-    for (i=k; i<strlen(str1);i++) {
-        if (strlen(str2)==1) {
+int main() {
+    char str1[100],str2[100];
+    int i,j=0,m=0,k=0,n,a=0;
+    gets(str1);
+    gets(str2);
+    if (strlen(str2)==1) {
+        for (i=k; i<strlen(str1);i++){
             if (str1[i]==str2[j]) {
                 m=m+1;
             }
         }
-        else if ((str1[i]==str2[j])&&((str1[i+1]==str2[j+1])||(str1[i-1]==str2[j-1]))) {
-            j=j+1;
+    }
+    else {
+        for (n=0;n<3;n++) { //无意义 只是预设循环数
+            for (i=k; i<strlen(str1);i++) {
+                if ((str1[i]==str2[j])&&((str1[i+1]==str2[j+1])||(str1[i-1]==str2[j-1]))) {
+                    m=m+1;
+                    j=j+1;
+                }
+                if (j==strlen(str2)) {
+                    k=i-j+2;
+                    a=a+1;
+                    j=0;
+                }
+            }
         }
-        if (j==strlen(str2)) {
-            k=k+1;
-            FUN(k,str1,str2);
-        }
     }
-    if (strlen(str2)==1) {
-        return m;
-    }
-    else
-        return k;
-}
-int main() {
-    char str1[100],str2[100];
-    int m,n,k=0;
-    gets(str1);
-    gets(str2);
-    if (strlen(str2)==1) {
-        m=FUN(k,str1,str2);
-    }
-    else
-        n=FUN(k,str1,str2);
-    if (n==0){
+    if (m<strlen(str2)){
         printf("NO\n");
     }
     else{
         if (strlen(str2)==1) {
-            printf("%d",m);
+            printf("%d\n",m);
         }
         else
-            printf("%d",n);
+            printf("%d\n",a);
     }
     return 0;
 }
