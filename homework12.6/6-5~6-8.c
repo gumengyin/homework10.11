afc856452cc2dcbf6e6d3b975fd7a369f188698f
gumengyin <gumegyin@163.com>
2016-12-13 00:50:08 +0800

    struct结构类型申明

---
+++ homework12.6/6-5~6-8.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  6-5~6-8
+//
+//  Created by 顾梦寅Joyce on 2016/12/13.
+//  Copyright © 2016年 顾梦寅Joyce. All rights reserved.
+//
+
+#include <stdio.h>
+int main() {
+    struct staff {
+        int sno;
+        char name[21];
+        char sex[3];
+        char identity[20];
+        int salary;
+        char address[100];
+    };
+    return 0;
+}
