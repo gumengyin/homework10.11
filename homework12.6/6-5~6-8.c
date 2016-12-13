2fe3ad800bd451351107bd3f51a21ee1c363b33f
gumengyin <gumegyin@163.com>
2016-12-13 23:51:57 +0800

    6-8

--- homework12.6/6-5~6-8.c
+++ homework12.6/6-5~6-8.c
@@ -15,14 +15,15 @@ int main() {
         char identity[20];
         int salary;
         char address[100];
+        char time[15];
     };
     int i,j;
     double sum=0,average;
     struct staff staff[8];
     struct staff temp;
     for (i=0; i<8; i++) {
-        scanf("%d %s %s %s %d %s",&staff[i].sno,staff[i].name,staff[i].sex,staff[i].identity,&staff[i].salary,staff[i].address);
-        printf("%d %s %s %s %d %s",staff[i].sno,staff[i].name,staff[i].sex,staff[i].identity,staff[i].salary,staff[i].address);
+        scanf("%d %s %s %s %d %s %s",&staff[i].sno,staff[i].name,staff[i].sex,staff[i].identity,&staff[i].salary,staff[i].address,staff[i].time);
+        printf("%d %s %s %s %d %s %s",staff[i].sno,staff[i].name,staff[i].sex,staff[i].identity,staff[i].salary,staff[i].address,staff[i].time);
     }
     for (i=0; i<8; i++) {
         sum=sum+staff[i].salary;
@@ -37,7 +38,7 @@ int main() {
             }
         }
     }
-    printf("\n工资最高\n%d %s %s %s %d %s",staff[0].sno,staff[0].name,staff[0].sex,staff[0].identity,staff[0].salary,staff[0].address);
+    printf("\n工资最高\n%d %s %s %s %d %s %s",staff[0].sno,staff[0].name,staff[0].sex,staff[0].identity,staff[0].salary,staff[0].address,staff[0].time);
     printf("\n工资总和：%.1f",sum);
     printf("\n平均工资：%.1f",average);
     return 0;
