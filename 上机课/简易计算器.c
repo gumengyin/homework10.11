de069d58da32aa3e449a678b88fd3aa8612787cf
gumengyin <gumegyin@163.com>
2016-12-13 23:19:03 +0800

    用字符串形式输入运算 考虑小数

--- 上机课/简易计算器.c
+++ 上机课/简易计算器.c
@@ -8,17 +8,26 @@
 
 #include <stdio.h>
 #include <string.h>
-double calculator (char *ptr);
+//double calculator (char *ptr); //如何把数字传给指针变量 若定义成指针数组 星号为什么要加
 int main() {
-    char digit[21];
-    int i,n,var1=0,var2=0;
+    char digit[20];
     gets(digit);
+    int i,n=0,m1=0,m2=0,flag;
+    double var1=0,var2=0,a=0.1;
     for (i=0; i<strlen(digit); i++) {
         if (digit[i]>=48&&digit[i]<=57) {
             digit[i]=digit[i]-'0';
         }
         else if (digit[i]==42||digit[i]==43||digit[i]==45||digit[i]==47) {
             n=i;
+            continue;
+        }
+        else if(digit[i]==46){
+            if (n==0) {
+                m1=i;
+            }
+            else
+                m2=i;
             continue;
         }
         else{
@@ -28,25 +37,52 @@ int main() {
         }
     }
     if (flag==0) {
-        for (i=0; i<n; i++) {
-            var1=var1*10+digit[i];
+        if (m1==0) {
+            for (i=0; i<n; i++) {
+                var1=var1*10+digit[i];
+            }
         }
-        for (i=n+1; i<strlen(digit); i++) {
-            var2=var2*10+digit[i];
+        if (m1!=0) {
+            for (i=0; i<m1; i++) {
+                var1=var1*10+digit[i];
+            }
+            for (i=m1+1; i<n; i++) {
+                var1=var1+digit[i]*a;
+                a=a*0.1;
+            }
+        }
+        a=0.1;
+        if (m2==0) {
+            for (i=n+1; i<strlen(digit); i++) {
+                var2=var2*10+digit[i];
+            }
+        }
+        if (m2!=0) {
+            for (i=n+1; i<m2; i++) {
+                var2=var2*10+digit[i];
+            }
+            for (i=m2+1; i<strlen(digit); i++) {
+                var2=var2+digit[i]*a;
+                a=a*0.1;
+            }
         }
         if (digit[n]==42) {
-            printf("%d\n",var1*var2);
+            printf("%.2f\n",var1*var2);
         }
         if (digit[n]==43) {
-            printf("%d\n",var1+var2);
+            printf("%.2f\n",var1+var2);
         }
         if (digit[n]==45) {
-            printf("%d\n",var1-var2);
+            printf("%.2f\n",var1-var2);
         }
         if (digit[n]==47) {
-            printf("%d\n",var1/var2);
+            printf("%.2f\n",var1/var2);
         }
     }
     return 0;
 }
+//double calculator (char *ptr){
+
+//return 0;
+//}
 //用字符形式输入数字 字符转数字 -48/-'0'
