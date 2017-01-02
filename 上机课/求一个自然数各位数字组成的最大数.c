c64c0046900d844009411276d24119dbb6f8b1ac
gumengyin <gumegyin@163.com>
2017-01-03 00:30:35 +0800

    优化输入gets的问题

--- 上机课/求一个自然数各位数字组成的最大数.c
+++ 上机课/求一个自然数各位数字组成的最大数.c
@@ -15,16 +15,19 @@ int main() {
         int j,k,temp;
         char a[100];
         gets(a);
-        for (j=0; j<strlen(a); j++) {
-            for (k=0; k<strlen(a)-j; k++) {
-                if (a[k]<a[k+1]) {
-                    temp=a[k];
-                    a[k]=a[k+1];
-                    a[k+1]=temp;
+        if (a[0]>0) {   //排除gets空输入 gets是以回车结尾,接收回车前的内容 所以输完n回车会输入一个空
+            for (j=0; j<strlen(a); j++) {
+                for (k=0; k<strlen(a)-j; k++) {
+                    if (a[k]<a[k+1]) {
+                        temp=a[k];
+                        a[k]=a[k+1];
+                        a[k+1]=temp;
+                    }
                 }
             }
+            printf("%s\n",a);
         }
-        printf("%s\n",a);
+        
     }
     return 0;
 }
