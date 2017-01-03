a055828b9ab842c06cf05651126f7031b39e8b94
gumengyin <gumegyin@163.com>
2017-01-03 16:11:28 +0800

    计算星期

---
+++ 上机课/计算星期.c
@@ -0,0 +1,40 @@
+//
+//  main.c
+//  判断星期
+//
+//  Created by 顾梦寅Joyce on 2017/1/3.
+//  Copyright © 2017年 顾梦寅Joyce. All rights reserved.
+//
+
+#include <stdio.h>
+int main() {
+    int day;
+    scanf("%d",&day);
+    switch (day) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Input Error\n");
+            break;
+    }
+    return 0;
+}
