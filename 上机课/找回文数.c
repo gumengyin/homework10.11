4ebacf25850253f22546ea10e9556340dca0eb63
gumengyin <gumegyin@163.com>
2017-01-03 16:01:58 +0800

    用字符串形式输入

--- 上机课/找回文数.c
+++ 上机课/找回文数.c
@@ -7,17 +7,31 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 int main() {
     int n,i;
     scanf("%d",&n);
     for (i=0; i<n; i++) {
-        int a,b,var,j;
-        scanf("%d%d",&a,&b);
-        a=a/10;
-        var=a%10;
-        a=a/10;
-        for (j=0; j<b; j++) {
-            printf("%d\n",a*100+(var+j)*10+a);
+        char a[10];
+        int b,j,k=0,l;
+        scanf("%s%d",a,&b);
+        int m=strlen(a)/2;
+        for (j=0; ;j++) {
+            a[m]=a[m]+1;
+            for (l=0; l<=m; l++) {
+                printf("%c",a[l]);
+            }
+            for (l=m-1; l>=0; l--) {
+                printf("%c",a[l]);
+            }
+            k=k+1;
+            if (k!=b) {
+                printf(" ");
+            }
+            if (k==b) {
+                printf("\n");
+                break;
+            }
         }
     }
     return 0;
